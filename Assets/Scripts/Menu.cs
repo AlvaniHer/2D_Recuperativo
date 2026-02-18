703eac4cdd64ffd59ac93c8eea4c3772aaf1d31b
AlvaniHer <99564929+AlvaniHer@users.noreply.github.com>
2026-02-18 19:10:47 +0100

    ultimo commit

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuPrincipal : MonoBehaviour
+public class Menu: MonoBehaviour
 {
     // Método para iniciar el juego
     public void Jugar()
     {
-        // Si el GameManager ya existe, reseteamos sus valores manualmente
         if (GameManager.instance != null)
         {
             GameManager.instance.vidas = 3;
-            // Si tienes una variable de manzanas en el manager, ponla a 0 también
             // GameManager.instance.manzanas = 0; 
         }
-        // Carga la escena del juego (asegúrate de que esté añadida en Build Settings)
-        SceneManager.LoadScene("SampleScene"); // Cambia por el nombre de tu escena de juego
+        // Carga la escena del juego 
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Método para mostrar créditos (si tienes otra escena)
+    // Método para reiniciar el juego
     public void Reinicio()
     {
         SceneManager.LoadScene("juego"); 
